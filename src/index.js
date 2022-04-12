43b9ecc40c2f4f7458afd2d0d541ee3f229ec472
Ademola <a.g.jegede@wlv.ac.uk>
2022-04-12 14:33:14 +0100

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bulma/css/bulma.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
+
   document.getElementById('root')
 );
