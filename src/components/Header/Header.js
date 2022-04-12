954c2ac123b17fe0bd56b35604a412143e405654
Ademola <a.g.jegede@wlv.ac.uk>
2022-04-12 14:32:50 +0100

    modified header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,20 +5,16 @@ function Header() {
   return (
     <div className="topbar">
     <div className="topbar-left">
-        <i className="topbar-social-icon fab fa-instagram"></i>
-        <i className="topbar-social-icon fab fa-facebook"></i>
-        <i className="topbar-social-icon fab fa-linkedin"></i>
-        <i className="topbar-social-icon fab fa-twitter"></i>
+    <p className='web'> WEB TECH COURSEWORK </p>
     </div>
     <div className="topbar-center">
         <ul className="topbar-list">
             <li className="topbar-list-item">HOME <i class="fa-solid fa-house"></i></li>
             <li className="topbar-list-item">ABOUT</li>
-            <li className="topbar-list-item"> ADD NEW</li>
         </ul>
     </div>
     <div className="topbar-right">
-        <i className="topbar-search fas fa-search"></i>
+    <li className="topbar-list-item"> ADD NEW</li>
     </div>
 </div>
   )
