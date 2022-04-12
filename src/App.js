4be366d254c86a6bcb006764bc3cba69e042fc28
Ademola <a.g.jegede@wlv.ac.uk>
2022-04-12 14:34:19 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
+import StudentList from './components/StudentList'
+import AddStudent from './components/AddStudent'
+import EditStudent from './components/EditStudent'
+
 
 function App() {
   return (
     <div>
-      <Header />
+    <Header />
+      <Router>
+          <div className='container'>
+          <Routes>
+          <Route exact path='/' element={<StudentList />} />
+          <Route exact path='/add' element={<AddStudent />} />
+          <Route exact path='/edit/:id' element={<EditStudent />} />
+          </Routes>
+
+      </div>
+    </Router>  
+    <Footer />
     </div>
   )
 }
 
 export default App
+
+
+
