5f54fda6c1524cecc07e8188b0f9a269e9ce2640
Ademola <a.g.jegede@wlv.ac.uk>
2022-04-12 15:23:31 +0100

    footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+    import React from 'react'
+    import './Footer.css'
+
+    function Footer() {
+    return (
+        <div className='foot'>
+            <p>© ADEMOLA 2122998 Msc Computer Science</p>
+        </div>
+    )
+    }
+
+    export default Footer
